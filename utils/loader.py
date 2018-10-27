c12062a9ff3ddfbb92a3ed3a1a3e28f822ac8900
Navdeep Chahal <navdeepchahal22@gmail.com>
2018-10-27 15:44:16 +0530

    Changed target to feature in test

--- utils/loader.py
+++ utils/loader.py
@@ -40,7 +40,7 @@ class TweetData(Dataset):
 		if self.split == 'test':
 			return {
 				'input': self.data[idx]['input'],
-				'feature': self.data[idx]['target'],
+				'feature': self.data[idx]['feature'],
 				'length': self.data[idx]['length'],
 				'feature_turn1': self.data[idx]['feature_turn1'],
 				'feature_turn2': self.data[idx]['feature_turn2'],
